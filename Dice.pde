99371ba973c4fca79a57df24b6bf281a62a39262
tianlunlee <tianlunlee@gmail.com>
2015-09-08 11:27:06 -0700

    dice

--- Dice.pde
+++ Dice.pde
@@ -1,45 +1,95 @@
+Die firstDie;
+int count;
+float vA;
+float vB;
+float vC;
 void setup()
 {
 	size(400, 400);
+	//background(25);
 	noLoop();
+
+
 }
 void draw()
 {
 	//your code here
-	background(25);
-	Die firstDie = new Die(50, 50);
+
+	firstDie = new Die(25, 25, 25);
 	firstDie.show();
 
 }
 void mousePressed()
 {
 	redraw();
-	count = (int)(Math.random()*6) + 1;
+	firstDie.roll();
+	firstDie.show();
 }
 class Die //models one single dice cube
 {
 	//variable declarations here
 	//constructor
-	int rectX, rectY;
-	Die(int x, int y) {
+	int rectX, rectY, rectS;
+	Die(int x, int y, int s) {
 		//variable initializations here
 		rectX = x;
 		rectY = y;
+		rectS = s;
 
 	}
 	void roll()
 	{
-
 		//your code here
+		count = (int)(Math.random()*6) + 1;
+
 		
 
 	}
 	void show()
 	{
 		//your code here
-		rect(rectX, rectY, 50, 50);
+		rect(rectX, rectY, rectS, rectS);
 		fill(0);
-		strokeWeight(6);
-		point(rectX + 25, rectY + 25);
+		strokeWeight(5* (rectS/25));
+		vA = rectX*0.25;
+		vB = rectY*0.75;
+		vC = rectX*0.5;
+		if(count == 1) {
+			point(rectS + vC , rectS + vC);
+		}
+		if (count == 2) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+		}
+
+		if (count == 3) {
+			point(rectS + vC, rectS + vC);
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+		}
+		if (count == 4) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+		}
+		if (count == 5){
+			point(rectS + vC, rectS + vC);
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+		}
+		if (count == 6) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+			point(rectS + vA, rectS + vC);
+			point(rectS + vB, rectS + vC);
+		}
+		strokeWeight(1);
+		fill(255);
+		}
 	}
-}
+
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -14,48 +14,101 @@ import java.io.IOException;
 
 public class Dice extends PApplet {
 
+Die firstDie;
+int count;
+float vA;
+float vB;
+float vC;
 public void setup()
 {
 	size(400, 400);
+	//background(25);
 	noLoop();
+
+
 }
 public void draw()
 {
 	//your code here
-	background(25);
-	Die firstDie = new Die(50, 50);
+
+	firstDie = new Die(25, 25, 25);
 	firstDie.show();
 
 }
 public void mousePressed()
 {
 	redraw();
+	firstDie.roll();
+	firstDie.show();
 }
 class Die //models one single dice cube
 {
 	//variable declarations here
 	//constructor
-	int rectX, rectY;
-	Die(int x, int y) {
+	int rectX, rectY, rectS;
+	Die(int x, int y, int s) {
 		//variable initializations here
 		rectX = x;
 		rectY = y;
+		rectS = s;
 
 	}
 	public void roll()
 	{
 		//your code here
+		count = (int)(Math.random()*6) + 1;
+
+		
 
 	}
 	public void show()
 	{
 		//your code here
-		rect(rectX, rectY, 50, 50);
+		rect(rectX, rectY, rectS, rectS);
 		fill(0);
-		strokeWeight(6);
-		point(rectX + 25, rectY + 25);
+		strokeWeight(5* (rectS/25));
+		vA = rectX*0.25f;
+		vB = rectY*0.75f;
+		vC = rectX*0.5f;
+		if(count == 1) {
+			point(rectS + vC , rectS + vC);
+		}
+		if (count == 2) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+		}
+
+		if (count == 3) {
+			point(rectS + vC, rectS + vC);
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+		}
+		if (count == 4) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+		}
+		if (count == 5){
+			point(rectS + vC, rectS + vC);
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+		}
+		if (count == 6) {
+			point(rectS + vB, rectS + vA);
+			point(rectS + vA, rectS + vB);
+			point(rectS + vA, rectS + vA);
+			point(rectS + vB, rectS + vB);
+			point(rectS + vA, rectS + vC);
+			point(rectS + vB, rectS + vC);
+		}
+		strokeWeight(1);
+		fill(255);
+		}
 	}
-}
+
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Dice" };
     if (passedArgs != null) {
